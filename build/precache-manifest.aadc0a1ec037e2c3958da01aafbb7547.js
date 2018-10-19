adad34cc1171d388d9057f55ac36571a93f4d518
Ian Kitchens <oziek88@gmail.com>
2018-10-19 21:05:08 +0200

    first commit

---
+++ build/precache-manifest.aadc0a1ec037e2c3958da01aafbb7547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66732d0038668b9006e2",
+    "url": "/static/js/main.66732d00.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "66732d0038668b9006e2",
+    "url": "/static/css/main.eb4985c5.chunk.css"
+  },
+  {
+    "revision": "a6e3f121d6bfb5f2dd0f29dec67caf60",
+    "url": "/index.html"
+  }
+];
---
+++ festfinder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FestfinderConfig(AppConfig):
+    name = 'festfinder'
